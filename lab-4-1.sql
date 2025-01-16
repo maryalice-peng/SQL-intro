fecad3b254eb07af8c9aa875714612c852c8cf27
MaryAlice Peng <maryalice.peng@gmail.com>
2025-01-16 03:00:28 +0000

    Lab 3s, attempt lab 4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,15 @@
 -- Expected result:
 -- 2935
 
+SELECT id, first_name, last_name
+FROM players
+WHERE first_name = "Barry"
+AND last_name = "Bonds"
 
+-- Barry Bonds id = 1678
+
+SELECT SUM(stats.hits)
+FROM players
+INNER JOIN stats ON players.id = stats.player_id 
+WHERE first_name = "Barry"
+AND last_name = "Bonds";
