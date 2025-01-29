508aa75294603e9ebe97e94ad25851691302a4c0
MaryAlice Peng <maryalice.peng@gmail.com>
2025-01-29 05:34:48 +0000

    Up to 50min of class 3

--- school-5-insert.sql
+++ school-5-insert.sql
@@ -6,4 +6,13 @@ INSERT INTO students (
     "MaryAlice",
     "Peng",
     "teehee@gmail.com"
+    );
+INSERT INTO students (
+    first_name,
+    last_name,
+    email
+) VALUES (
+    "Preston",
+    "Fegley",
+    "prettypreston@gmail.com"
 );
---
+++ school-6-update.sql
@@ -0,0 +1,6 @@
+UPDATE students 
+SET email = "teehee@gmail.com"
+WHERE id = 1;
+UPDATE students 
+SET email = "prettypreston@gmail.com"
+WHERE id = 3;
